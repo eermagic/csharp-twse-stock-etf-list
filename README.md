924167958e552e86a456edaef74dbe8ed4ac6e2e
Mars <eermagic@yahoo.com.tw>
2022-10-12 23:00:41 +0800

    First Update

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # csharp-twse-stock-etf-list
 [C#] 取得證交所上市及上櫃的股票及ETF清單
+
+在證券交易所及櫃買中心市場上每一天都會有上市或下市的有價證券清單，做程式交易除了每日更新股價之外，還需要定期更新一下新上市或已下市的有價證券清單。
+在證交所有一個網址可以查看最新上市或上櫃的清單。
+### 查詢上市清單網址
+
+https://isin.twse.com.tw/isin/C_public.jsp?strMode=2
+![](https://blog.hungwin.com.tw/wp-content/uploads/2021/05/csharp-twse-stock-etf-list-2.jpg)
+
+### 查詢上櫃清單網址
+
+https://isin.twse.com.tw/isin/C_public.jsp?strMode=4
+![](https://blog.hungwin.com.tw/wp-content/uploads/2021/05/csharp-twse-stock-etf-list-3.jpg)
+
+透過這 2 個網址就可以知道最新的上市或上櫃有價證券清單是什麼。
+可是這清單實在是太長了，包含了眾多的有價證券類別，如果只想看最常見的股票及 ETF 清單，
+那就需要用程式把清單中只屬於股票及 ETF 的清單篩選出來。
+
+接下來我會示範一下如何利用 C# 取得證交所有價證券清單的上市及上櫃股票 ETF 清單。
+
+[完整文章連結](https://blog.hungwin.com.tw/csharp-twse-stock-etf-list/)
---
+++ TeachGetTwseStockList/Controllers/HomeController.cs
@@ -0,0 +1,200 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using static TeachGetTwseStockList.Models.HomeModel;
+
+namespace TeachGetTwseStockList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// 取得證交所上市及上櫃的股票及ETF清單
+        /// </summary>
+        /// <param name="inModel"></param>
+        /// <returns></returns>
+        public ActionResult GetList(GetListIn inModel)
+        {
+            GetListOut outModel = new GetListOut();
+
+            // 檢查輸入來源
+            if (string.IsNullOrEmpty(inModel.Q_MARKET_TYPE))
+            {
+                outModel.ErrMsg = "請輸入市場別";
+                return Json(outModel);
+            }
+            if (string.IsNullOrEmpty(inModel.Q_ASSETS_TYPE))
+            {
+                outModel.ErrMsg = "請輸入資產別";
+                return Json(outModel);
+            }
+
+            string url = "";
+            string codeName = "";
+            string stockCode = "";
+            string stockName = "";
+            string marketType = "";
+            string industry = "";
+            string publicDate = "";
+            string assetType = "";
+            if (inModel.Q_MARKET_TYPE == "TWSE")
+            {
+                outModel.CName = "證交所";
+                // 來源網址
+                url = "https://isin.twse.com.tw/isin/C_public.jsp?strMode=2";
+            }
+            else if (inModel.Q_MARKET_TYPE == "OTC")
+            {
+                outModel.CName = "櫃買中心";
+                // 來源網址
+                url = "https://isin.twse.com.tw/isin/C_public.jsp?strMode=4";
+            }
+
+            WebClient webClient = new WebClient();
+            MemoryStream ms = new MemoryStream(webClient.DownloadData(url));
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(ms, Encoding.Default);
+            // 取得 HTML 標籤
+            HtmlNodeCollection trNode = doc.DocumentNode.SelectNodes("//table[2]/tr");
+            if (trNode != null)
+            {
+                // 建立輸出table
+                DataTable dtTwseStockList = new DataTable();
+                dtTwseStockList.Columns.Add("SYMBOL_CODE");
+                dtTwseStockList.Columns.Add("SYMBOL_NAME");
+                dtTwseStockList.Columns.Add("MARKET_TYPE");
+                dtTwseStockList.Columns.Add("ASSETS_TYPE");
+                dtTwseStockList.Columns.Add("PUBLIC_DATE");
+                dtTwseStockList.Columns.Add("INDUSTRY");
+
+                foreach (HtmlNode tr in trNode)
+                {
+                    stockCode = "";
+                    HtmlNodeCollection tdNode = tr.SelectNodes("td");
+                    if (tdNode.Count == 7)
+                    {
+                        string stockType = tdNode[5].InnerText;
+                        if (inModel.Q_MARKET_TYPE == "TWSE" && inModel.Q_ASSETS_TYPE == "STOCK")
+                        {
+                            // 取得證交所股票
+                            if (stockType == "ESVUFR")
+                            {
+                                codeName = tdNode[0].InnerText;
+                                stockCode = codeName.Split('　')[0];
+                                stockName = codeName.Split('　')[1];
+                                publicDate = tdNode[2].InnerText;
+                                marketType = tdNode[3].InnerText;
+                                industry = tdNode[4].InnerText;
+
+                                assetType = "股票";
+                            }
+                        }
+                        else if (inModel.Q_MARKET_TYPE == "TWSE" && inModel.Q_ASSETS_TYPE == "ETF")
+                        {
+                            // 取得證交所ETF
+                            if (stockType == "CEOGEU" || stockType == "CEOGDU" || stockType == "CEOGMU" || stockType == "CEOJEU" || stockType == "CEOIBU" || stockType == "CEOJLU" || stockType == "CEOGBU" || stockType == "CEOIEU" || stockType == "CEOGCU" || stockType == "CEOIRU")
+                            {
+                                codeName = tdNode[0].InnerText;
+                                stockCode = codeName.Split('　')[0];
+                                stockName = codeName.Split('　')[1];
+                                publicDate = tdNode[2].InnerText;
+                                marketType = tdNode[3].InnerText;
+                                industry = tdNode[4].InnerText;
+                                assetType = "ETF";
+                            }
+                        }
+                        else if (inModel.Q_MARKET_TYPE == "OTC" && inModel.Q_ASSETS_TYPE == "STOCK")
+                        {
+                            // 取得櫃買中心股票
+                            if (stockType == "ESVUFR")
+                            {
+                                codeName = tdNode[0].InnerText;
+                                stockCode = codeName.Split('　')[0];
+                                stockName = codeName.Split('　')[1];
+                                publicDate = tdNode[2].InnerText;
+                                marketType = tdNode[3].InnerText;
+                                industry = tdNode[4].InnerText;
+
+                                assetType = "股票";
+                            }
+
+                        }
+                        else if (inModel.Q_MARKET_TYPE == "OTC" && inModel.Q_ASSETS_TYPE == "ETF")
+                        {
+                            // 取得櫃買中心ETF
+                            if (stockType == "CEOGBU" || stockType == "CEOGEU" || stockType == "CEOIBU" || stockType == "CEOIEU" || stockType == "CEOIRU" || stockType == "CEOJBU")
+                            {
+                                codeName = tdNode[0].InnerText;
+                                stockCode = codeName.Split('　')[0];
+                                stockName = codeName.Split('　')[1];
+                                publicDate = tdNode[2].InnerText;
+                                marketType = tdNode[3].InnerText;
+                                industry = tdNode[4].InnerText;
+                                assetType = "ETF";
+                            }
+                        }
+
+                        if (stockCode != "")
+                        {
+                            // 加入 datatable
+                            DataRow drNew = dtTwseStockList.NewRow();
+                            drNew["SYMBOL_CODE"] = stockCode;
+                            drNew["SYMBOL_NAME"] = stockName;
+                            drNew["MARKET_TYPE"] = marketType;
+                            drNew["ASSETS_TYPE"] = assetType;
+                            drNew["PUBLIC_DATE"] = publicDate;
+                            drNew["INDUSTRY"] = industry;
+                            dtTwseStockList.Rows.Add(drNew);
+                        }
+
+                    }
+                }
+
+                // 輸出資料
+                outModel.gridList = new List<StockRow>();
+                foreach (DataRow dr in dtTwseStockList.Rows)
+                {
+                    StockRow row = new StockRow();
+                    row.SYMBOL_CODE = dr["SYMBOL_CODE"].ToString();
+                    row.SYMBOL_NAME = dr["SYMBOL_NAME"].ToString();
+                    row.MARKET_TYPE = dr["MARKET_TYPE"].ToString();
+                    row.ASSETS_TYPE = dr["ASSETS_TYPE"].ToString();
+                    row.PUBLIC_DATE = dr["PUBLIC_DATE"].ToString();
+                    row.INDUSTRY = dr["INDUSTRY"].ToString();
+                    outModel.gridList.Add(row);
+                }
+                outModel.RowCnt = dtTwseStockList.Rows.Count;
+            }
+
+            // 回傳 Json 給前端
+            return Json(outModel);
+        }
+    }
+}
